46c991036b0de4d7657dd510e6ace824bae3c97c
oim1 <oim.oom.imo@gmail.com>
2025-02-18 13:54:25 +1100

    Challenge 5

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 
 console.log("Hello World");
 
+const myJSON = {"message": "Hello json"};
+
 app.get("/", function(req,res){
  let absolutePath = __dirname + "/views/index.html";
  res.sendFile(absolutePath);
@@ -11,6 +13,10 @@ app.get("/", function(req,res){
 let cssPath = __dirname + "/public";
 
 app.use("/public", express.static(cssPath));
+
+app.get("/json", function(req,res){
+ res.jsonp(myJSON);
+})
 
 
 
